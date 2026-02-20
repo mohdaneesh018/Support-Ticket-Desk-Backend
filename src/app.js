35af9bd88a89cc69dc4d5175796dc7d2be25c221
mohdaneesh018 <mdaneesh537@gmail.com>
2026-02-21 00:45:59 +0530

    Updated Frontend URL

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import errorHandler from "./middlewares/error.middleware.js";
 const app = express();
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://support-ticket-desk-frontend.vercel.app/",
         credentials: true,
     })
 );
